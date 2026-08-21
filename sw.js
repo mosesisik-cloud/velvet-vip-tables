58f2447e39cfea4268b6292c1ca7562aeb582445
Moses Isik <moses.isik@alimenta2017.local>
2026-08-21 06:07:58 +0200

    Ikoner star stilla vid bladdring: ingen sidledsscroll

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 "use strict";
 
-const VERSION = "v4-real-24";
+const VERSION = "v4-real-25";
 const SHELL_CACHE = "velvet-shell-" + VERSION;
 const RUNTIME_CACHE = "velvet-runtime-" + VERSION;
 
