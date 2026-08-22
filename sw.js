f8f754b2d8b38b5ea2a3a213df2002311272d422
gabrielhadodo9602 <gabrielhadodo@gmail.com>
2026-08-22 10:46:19 +0000

    Add tiered venue package selector

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 "use strict";
 
-const VERSION = "v4-real-52";
+const VERSION = "v4-real-53";
 const SHELL_CACHE = "velvet-shell-" + VERSION;
 const RUNTIME_CACHE = "velvet-runtime-" + VERSION;
 
