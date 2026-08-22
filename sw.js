e7c5672cdf60d287d0c32c82a46050d21fdfefe4
mosesisik-cloud <mosesisik@gmail.com>
2026-08-22 16:56:00 +0200

    SW v70: nytt skal med IDV-watchdogen

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 "use strict";
 
-const VERSION = "v4-real-69";
+const VERSION = "v4-real-70";
 const SHELL_CACHE = "velvet-shell-" + VERSION;
 const RUNTIME_CACHE = "velvet-runtime-" + VERSION;
 
