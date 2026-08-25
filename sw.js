3e3c64195ef34ffd7d100fc37a06183566de9196
Gabriel Hadodo <gabrielhadodo@MacBook-Air-som-tillhor-Gabriel.local>
2026-08-25 20:09:06 +0200

    Keep all sign-in options usable in preview mode

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 "use strict";
 
-const VERSION = "v4-real-104";
+const VERSION = "v4-real-105";
 const SHELL_CACHE = "velvet-shell-" + VERSION;
 const RUNTIME_CACHE = "velvet-runtime-" + VERSION;
 
