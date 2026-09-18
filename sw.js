b1dd2bef08c3d70616622e24e0a3f591d1799ac6
gabrielhadodo9602 <gabrielhadodo@gmail.com>
2026-09-18 14:37:49 +0200

    Release VELVET v151 with restored Zink originals

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 "use strict";
 
-const VERSION = "v4-real-150-stockholm-restaurants";
+const VERSION = "v4-real-151-zink-originals";
 const SHELL_CACHE = "velvet-shell-" + VERSION;
 const RUNTIME_CACHE = "velvet-runtime-" + VERSION;
 
